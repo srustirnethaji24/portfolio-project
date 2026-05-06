6e733dabf872a2234549cec32beadd21ae478088
srustirnethaji24 <srustirnethaji24@gmail.com>
2026-05-06 19:59:19 +0530

    Protfolio-project

--- js/data/skills.js
+++ js/data/skills.js
@@ -1,55 +1,41 @@
 const skillsData = [
   {
     id: 1,
-    name: "ReactJS",
-    shortLabel: "R",
-    category: "Frontend",
-    description: "Advanced Hooks, component-based UI, and state management.",
-  },
-  {
-    id: 2,
     name: "Tailwind CSS",
     shortLabel: "T",
     category: "Frontend",
     description: "Responsive layouts, utility-first styling, and clean UI building."
   },
   {
-    id: 3,
+    id: 2,
     name: "JavaScript",
     shortLabel: "J",
     category: "Frontend",
     description: "DOM manipulation, events, validation, Fetch API, and storage."
   },
   {
-    id: 4,
+    id: 3,
     name: "MongoDB",
     shortLabel: "M",
     category: "Backend",
     description: "Document-based database design and backend integration."
   },
   {
-    id: 5,
+    id: 4,
     name: "HTML",
     shortLabel: "H",
     category: "Frontend",
     description: "HTML structures content on the web using tags and elements."
   },
   {
-    id: 6,
-    name: "ExpressJS",
-    shortLabel: "E",
-    category: "Backend",
-    description: "Backend framework for Node.js to build APIs."
-  },
-  {
-    id: 7,
+    id: 5,
     name: "NodeJS",
     shortLabel: "N",
     category: "Backend",
     description: "Runtime environment to execute JavaScript on server."
   },
   {
-    id: 8,
+    id: 6,
     name: "SQL",
     shortLabel: "S",
     category: "Database",
